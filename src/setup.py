dedf36f501df5106fe683c60f8f08f27072cad9d
atvcaptain <openatv@gmail.com>
2026-04-02 21:37:26 +0200

    enable locate

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 import setup_translate
 
-pkg = 'Extensions.VuChromium'
+pkg = 'Extensions.Chromium'
 setup(name='enigma2-plugin-extensions-chromium',
        version='3.0',
        description='E2 Chromium Plugin',
-       package_dir={pkg: 'VuChromium'},
+       package_dir={pkg: 'Chromium'},
        packages=[pkg],
        package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass=setup_translate.cmdclass,  # for translation
